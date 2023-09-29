d1965dd7ee1a78ddf824982a4aa45ec33beef7c1
Thales Lima <lthales53@gmail.com>
2023-09-28 21:54:04 -0300

    Implementando a camada de Application

---
+++ src/NerdStore.Catalogo.Application/AutoMapper/DtoToDomainMappingProfile.cs
@@ -0,0 +1,19 @@
+using AutoMapper;
+using NerdStore.Catalogo.Application.Dtos;
+using NerdStore.Catalogo.Domain;
+
+namespace NerdStore.Catalogo.Application.AutoMapper;
+
+public class DtoToDomainMappingProfile : Profile
+{
+    public DtoToDomainMappingProfile()
+    {
+        CreateMap<ProdutoDto, Produto>()
+            .ConstructUsing(p => 
+                new Produto(p.Nome, p.Descricao, p.Ativo, p.Valor, p.DataCadastro, p.Imagem, p.CategoriaId, 
+                    new Dimensoes(p.Altura, p.Largura, p.Profundidade)));
+        
+        CreateMap<CategoriaDto, Categoria>()
+            .ConstructUsing(c => new Categoria(c.Nome, c.Codigo));
+    }
+}
